583db7e0a5b80be44488c01cc5324d007045fdb1
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-28 00:15:24 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,27 +6,12 @@ class Anagram
   attr_accessor :name
   
   def initialize(word)
-<<<<<<< HEAD
-    @word = word
-  end  
-  
-  def self.match(anagram)
-    matches = []
-    anagram.split("").sort.join == %w(@word).split("").sort.join
-      matches << 
-    else
-      []
-    end  
-  end    
-end    
-=======
     @name = word
   end 
 
   def match(array)
     array.select {|x| x.split("").sort == @name.split("").sort}
   end   
->>>>>>> c93cf642b46012cd02ff61bc5fdd24925c072e6a
 
 end 
 
